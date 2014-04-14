51fc16ae8073829f67c7837c49e3e36be1487934
kraiz <der.kraiz@gmail.com>
2014-04-14 23:17:10 +0200

    added local development server

---
+++ server.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+#
+# Simple server for local development.
+# - serves the app folder via http
+# - requests to "/rpc" will be proxied to http://192.168.1.122
+#   (where the nginx proxy it into eiskalt-daemon)
+#
+import os
+from twisted.internet import reactor
+from twisted.web import proxy, static, server
+
+root = static.File(os.path.join(os.path.dirname(__file__), 'app'))
+root.putChild("rpc", proxy.ReverseProxyResource('192.168.1.122', 80, '/rpc'))
+
+reactor.listenTCP(80, server.Site(root))
+reactor.run()
