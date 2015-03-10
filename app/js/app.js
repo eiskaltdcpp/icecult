060f68beb5a11306fa796ed013bf3641e2020af9
kraiz <lars.kreisz@gmail.com>
2015-03-10 13:30:56 +0100

    release new version 0.5.1

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@ var EiskaltApp = angular.module('EiskaltApp', ['ngRoute', 'ngStorage', 'ngSaniti
                                                'angularBootstrapNavTree', 'UpdateCheck', 'EiskaltRPC', 'EiskaltFilters']);
 
 EiskaltApp.value('settings', {
-    version: '0.5.0',
+    version: '0.5.1',
     localStorageVersion: 2,
     updateUrl: 'https://api.github.com/repos/kraiz/icecult/releases',
     chatMessagesKept: 250,
