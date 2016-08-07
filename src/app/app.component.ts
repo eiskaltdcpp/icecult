304c699d58c7c49c6a276f6a84f9530f6a90b97c
kraiz <lars.kreisz@gmail.com>
2016-08-07 20:31:09 +0200

    imported angular2-webpack seed project

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
+
+import { ApiService } from './shared';
+
+import '../style/app.scss';
+
+/*
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'my-app', // <my-app></my-app>
+  providers: [ApiService],
+  directives: [...ROUTER_DIRECTIVES],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  url = 'https://github.com/preboot/angular2-webpack';
+
+  constructor(private api: ApiService) {
+  }
+}
